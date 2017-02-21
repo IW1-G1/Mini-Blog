d71657c3c2be9f95d54d6ce49852cc7e55430e2d
Sylvain CABIATI <Sylvain CABIATI>
2017-02-21 18:30:13 +0100

    Ajout d'une fonction de redirection si l'utilisateur n'est pas admin

--- conf.inc.php
+++ conf.inc.php
@@ -5,3 +5,18 @@
   define("DB_USER","root");
   define("DB_PWD","");
   define("CSS_PATH","/Mini-Blog/public/css/");
+
+
+  function redirectNotAdmin(){
+    $auth = new Authentification();
+    if($auth->isConnected()){
+      $bdd = new PDO('mysql:host=localhost;dbname=mini_blog;charset=utf8', 'root', '');
+      $statement = $bdd->prepare("SELECT id FROM mb_users WHERE username = :username");
+      $statement->execute(array(':username' => $_SESSION['pseudo']));
+      $userId = $statement->fetchAll();
+        if(!$auth->isAdmin($userId)){
+          header("Location: ../index.php");
+          die();
+        }
+    }
+  }
